23ca350473bf148aed2b9293e9c5d571f680eaa8
Alexandre Page <alexandrepage@MacBook-Pro-de-Alexandre.local>
2024-09-05 15:39:30 +0200

    feat(signup): css for signup

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,8 +1,14 @@
+<script>
+import { RouterLink } from 'vue-router'
+
+</script>
 <template>
 			<header class="text-center my-8">
 				<h1 class="text-blue-300 text-6xl font-bold mb-4" @click="$router.push('/')">
 				Alex's blog
 				</h1>
 				<p class="text-xl text-slate-500">A blog with post on what I like</p>
+				<RouterLink to="/signup">Signup </RouterLink>
+				<RouterLink to="/signin"> Signin</RouterLink>
 			</header>
 </template>
--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -4,7 +4,6 @@ let password = ''
 let passwordError = ''
 
 const handleSubmit = () => {
-	console.log('hello')
 	passwordError = password.length > 5 ? '' : 'Password must be at least 5 chars long'
 }
 </script>
@@ -25,3 +24,74 @@ const handleSubmit = () => {
 		</form>		
 	</div>
 </template>
+
+<style type="text/css">
+
+	.error {
+	    color: #ff0062;
+	    margin-top: 10px;
+	    font-size: 0.8em;
+	    font-weight: bold;
+	}
+
+	button {
+    background: #0b6dff !important;
+    border: 0 !important;
+    padding: 10px 20px !important;
+    margin-top: 20px !important;
+    color: white !important;
+    border-radius: 20px !important;
+}
+.submit {
+    text-align: center;
+}
+
+	.pill {
+    display: inline-block;
+    margin: 20px 10px 0 0;
+    padding: 6px 12px;
+    background: #eee;
+    border-radius: 20px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+    color: #777;
+    cursor: pointer;
+  }
+	
+	form {
+		width: 420px;
+		margin: 30px auto;
+		background: white;
+		text-align: left;
+		padding: 40px;
+		border-radius: 10px;
+	}
+
+	label {
+		color: #aaa;
+		display: inline-block;
+		margin: 25px 0 15px;
+		font-size: 0.6em;
+		text-transform: uppercase;
+		letter-spacing: 1px;
+		font-weight: bold;
+	}
+
+	input, select {
+		display: block;
+		padding: 10px 6px;
+		width: 100%;
+		box-sizing: border-box;
+		border: none;
+		border-bottom: 1px solid #ddd;
+		color: #555;
+	}
+	input[type="checkbox"] {
+		display: inline-block;
+		width: 16px;
+		margin: 0 10px 0 0;
+		position: relative;
+		top: 2px;
+	}
+</style>
